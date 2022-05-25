1be65b571f156b848378a51aa1722fe3d695e6b1
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-05-25 18:14:50 -0300

    criação do banco de dados

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -4,10 +4,14 @@ import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 class CreateCategoryController {
     constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-    handle(request: Request, response: Response): Response {
+    async handle(request: Request, response: Response): Promise<Response> {
         const { name, description } = request.body;
 
-        this.createCategoryUseCase.execute({ name, description });
+        try {
+            await this.createCategoryUseCase.execute({ name, description });
+        } catch (err) {
+            return response.status(400).json({message: (err as Error).message});
+        }        
 
         return response.status(201).send();
     }
