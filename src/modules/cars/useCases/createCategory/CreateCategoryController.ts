a2c953cbe1c32556e3d4ac41c9762bcf79f78b37
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-05-26 00:28:38 -0300

    refatoração das especificações

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,17 +1,14 @@
 import { Response, Request } from 'express'
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+import { container } from 'tsyringe'
 
 class CreateCategoryController {
-    constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
-
+    
     async handle(request: Request, response: Response): Promise<Response> {
         const { name, description } = request.body;
+        const createCategoryUseCase = container.resolve(CreateCategoryUseCase)
 
-        try {
-            await this.createCategoryUseCase.execute({ name, description });
-        } catch (err) {
-            return response.status(400).json({message: (err as Error).message});
-        }        
+        await createCategoryUseCase.execute({ name, description });    
 
         return response.status(201).send();
     }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 import express from 'express';
 import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 import "./database";
+import "./shared/container"
 
 const app = express();
 app.use(express.json());
