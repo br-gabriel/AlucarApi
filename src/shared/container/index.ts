dd88760414a10d39f600edb21a2108c97db535f8
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-06-29 20:52:25 -0300

    User Repository Container added

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,6 +3,9 @@ import { ICategoriesRepository } from '../../modules/cars/repositories/ICategori
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { IUserRepository } from '../../modules/accounts/repositories/IUsersRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository", //definição do nome do container
@@ -12,4 +15,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
+)
+
+container.registerSingleton<IUserRepository>(
+    "UsersRepository",
+    UsersRepository
 )
