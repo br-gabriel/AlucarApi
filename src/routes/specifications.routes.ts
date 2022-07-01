de2c3b8504203c16aac20134b6ba985e483d7885
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-07-01 20:02:57 -0300

    authenticated routes added

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate"
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticate)
 specificationsRoutes.post("/", createSpecificationController.handle)
 
 export { specificationsRoutes }
