64ebc424195c8e761fca47aa08037770f2a360a0
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-07-01 19:53:27 -0300

    authentication route added

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,59 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+import { IUserRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UserRepository")
+        private usersRepository: IUserRepository
+    ){}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        //Verifica se o usuário existe
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        //Verifica se a senha está correta
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw new Error("Email or password incorrect!")
+        }
+
+        //Gerar jsonwebtoken
+        const token = sign({}, "698dc19d489c4e4db73e28a713eab07b", {
+            subject: user.id,
+            expiresIn: "1d"
+        })
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            }
+        }
+
+        return tokenReturn;
+    }
+}
+
+export { AuthenticateUserUseCase }
