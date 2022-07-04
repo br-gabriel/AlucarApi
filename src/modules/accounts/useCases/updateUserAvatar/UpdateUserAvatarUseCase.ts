f8d6f3aabe0a79a8ce902f8d16e14c7e6b624a2e
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-07-04 18:30:28 -0300

    old avatar delete function added

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUserRepository } from "../../repositories/IUsersRepository";
+
 
 interface IRequest {
     user_id: string;
@@ -15,6 +17,10 @@ class UpdateUserAvatarUseCase {
 
     async execute({user_id, avatar_file}: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
+
+        if(user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`)
+        }
 
         user.avatar = avatar_file;
 
