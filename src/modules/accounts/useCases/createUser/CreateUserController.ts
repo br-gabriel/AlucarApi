18fa97193589dd952dff84fe258c2fc9cd6669a2
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-07-05 18:06:51 -0300

    username removed

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,7 @@ class CreateUserController {
     async handle(request: Request, response: Response): Promise<Response> {
         const {
             name, 
-            username, 
+            //username, 
             email, 
             password, 
             driver_license
@@ -16,7 +16,7 @@ class CreateUserController {
 
         await createUserUseCase.execute({
             name,
-            username,
+            //username,
             email,
             password,
             driver_license,
